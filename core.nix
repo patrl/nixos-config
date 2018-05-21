57de5e3c3fa9c0ab8c4bd9229d8afb576dfed642
Félix Baylac-Jacqué <felix@alternativebit.fr>
2018-05-21 19:48:20 +0200

    Forgot some WG config...

--- core.nix
+++ core.nix
@@ -1,13 +1,10 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 { config, pkgs, ... }:
 
 let 
   keys = import ./keys.nix;
 in {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./packages.nix
       ./software-configuration.nix
     ];
@@ -15,23 +12,15 @@ in {
   nixpkgs.config = {
     allowUnfree = true;
   };
-  # Pulse audio
+
   hardware.pulseaudio.enable = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Select internationalisation properties.
   i18n = {
      consoleFont = "Lat2-Terminus16";
      consoleKeyMap = "fr-bepo";
      defaultLocale = "fr_FR.UTF-8";
   };
 
-  # Set your time zone.
   time.timeZone = "Europe/Paris";
 
   programs.zsh = {
@@ -47,19 +36,6 @@ in {
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
     layout = "fr";
@@ -70,23 +46,4 @@ in {
         udiskie -Ns &
     '';
   };
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-  #
-  #
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.extraUsers.guest = {
-  #   isNormalUser = true;
-  #   uid = 1000;
-  # };
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
 }
--- thinkpad.nix
+++ thinkpad.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports =
@@ -8,11 +8,14 @@
     ];
 
   # Use the GRUB 2 boot loader.
-  boot.loader.grub = {
-    enable = true;
-    enableCryptodisk = true;
-    version = 2;
-    device = "/dev/sda";
+  boot = {
+    extraModulePackages = [ config.boot.kernelPackages.wireguard ];
+    loader.grub = {
+       enable = true;
+       enableCryptodisk = true;
+       version = 2;
+       device = "/dev/sda";
+    };
   };
 
   networking = {
@@ -21,12 +24,23 @@
     networkmanager.enable = true;
   };
 
+  networking.wireguard.interfaces.wg0 = {
+    privateKey = builtins.readFile ./wgprivatekey;
+    ips = ["192.168.3.3"];
+    peers = [{
+      endpoint = "alternativebit.fr:51820";
+      publicKey = "YLIeSCLTy4PP5CkTp84lb52XWcYcz7S0SHkl7q2JVVQ=";
+      allowedIPs = ["0.0.0.0/0"  "::/0"];
+      persistentKeepalive = 25;
+    }];
+  };
+
   users.extraUsers.ninjatrappeur= {
       isNormalUser = true;
       home = "/home/ninjatrappeur";
       extraGroups = [ "networkmanager" "wheel" "audio" "video" "storage" "optical" "wireshark" ];
       shell = pkgs.zsh;
- };
+  };
 
   services.xserver= {
     libinput = {
